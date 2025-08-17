0ec4ea10da6b813a8daf083b4764d57f9fd94e77
Your Name <you@example.com>
2025-08-17 15:23:52 +0700

    add meta tags

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -7,11 +7,23 @@ export default defineConfig({
   html: {
     title: 'OpenAPI / Swagger visualizer',
     meta: ({ value }) => {
+      value['og:title'] = 'OpenAPI / Swagger visualizer';
+      value['twitter:title'] = 'OpenAPI / Swagger visualizer';
       value.description =
         'Easily visualize complex OpenAPI / Swagger API structure';
+      value['og:description'] =
+        'Easily visualize complex OpenAPI / Swagger API structure';
+      value['twitter:description'] =
+        'Easily visualize complex OpenAPI / Swagger API structure';
       value.keywords = 'OpenAPI, Swagger, visualizer, API structure';
+      value['og:image'] = 'https://openapi-visualizer.vercel.app/og-image.png';
+      value['twitter:image'] =
+        'https://openapi-visualizer.vercel.app/og-image.png';
+      value['og:url'] = 'https://openapi-visualizer.vercel.app/';
+      value['og:type'] = 'website';
     },
     favicon: './public/favicon.ico',
+    template: './index.html',
   },
   plugins: [pluginReact(), pluginChunksPreload()],
   // This is only affect in dev mode :(
